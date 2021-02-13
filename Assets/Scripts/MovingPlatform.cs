59b2ae08e8e50e292789ea19085f515dad749b11
ChrisPfeuffer <chris_pfeuffer@web.de>
2021-02-13 19:21:25 +0100

    Deletet comments in script

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -39,10 +39,6 @@ public class MovingPlatform : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// OnTriggerExit is called when the Collider other has stopped touching the trigger.
-    /// </summary>
-    /// <param name="other">The other Collider involved in this collision.</param>
     void OnTriggerExit(Collider other)
     {
         if(other.tag == "Player"){
