bbbe656b19aab48d0514866d0a4cbd8502041619
ChrisPfeuffer <chris_pfeuffer@web.de>
2021-02-21 13:24:51 +0100

    Player will respawn after falling of the Map and will lose 1 live

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class Player : MonoBehaviour
     [SerializeField] bool _canDoubleJump = false;
     [SerializeField] int _coins;
     UIManager _uiManager;
+    [SerializeField] int _lives = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +25,7 @@ public class Player : MonoBehaviour
             Debug.Log("UIManager is null");
         }
 
+        _uiManager.UpdateLivesDisplay(_lives);
     }
 
     // Update is called once per frame
@@ -66,4 +69,12 @@ public class Player : MonoBehaviour
         _uiManager.UpdateCoinDisplay(_coins);
     }
 
+    public void Damage(){
+        _lives--;
+        _uiManager.UpdateLivesDisplay(_lives);
+
+        if(_lives < 1){
+            SceneManager.LoadScene(0);
+        }
+    }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,8 +6,13 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] Text _coinText;
-    
+    [SerializeField] Text _livesText;
+
     public void UpdateCoinDisplay(int coins){
         _coinText.text = "Coins: " + coins.ToString();
+    }
+
+    public void UpdateLivesDisplay(int lives){
+        _livesText.text = "Lives: " + lives.ToString();
     }
 }
